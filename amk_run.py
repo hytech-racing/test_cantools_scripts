73aad4d5524a80ac5680c702c036ccdd517857dd
Mark <mmgoltsman@mail.ru>
2025-04-26 21:49:03 -0400

    working fake_vcf code setting accel to 0.01

--- amk_run.py
+++ amk_run.py
@@ -10,75 +10,26 @@ from enum import Enum
 bus1 = can.Bus(channel="can0", interface='socketcan')
 
 def main():
-    inverter_ready = False
-    inverter_hv_on = False # quit dc on
-    inverter_enabled = False # quit inverter on
+    inverter_on = False
+    recv = False
 
-    db = cantools.database.load_file("hytech_dbc.dbc")
-    setpoint = db.get_message_by_name("MC1_SETPOINTS_COMMAND")
-
-    empty_msg = setpoint.encode({'negative_torque_limit': 0, 'positive_torque_limit': 0, 'speed_setpoint_rpm': 0, 'remove_error': 1, 'driver_enable': 0, 'hv_enable': 0, 'inverter_enable': 0})
-    reset_error_msg = setpoint.encode({'negative_torque_limit': 0, 'positive_torque_limit': 0, 'speed_setpoint_rpm': 0, 'remove_error': 1, 'driver_enable': 0, 'hv_enable': 0, 'inverter_enable': 0})
-    hv_on_msg = setpoint.encode({'negative_torque_limit': 0, 'positive_torque_limit': 0, 'speed_setpoint_rpm': 0, 'remove_error': 0, 'driver_enable': 0, 'hv_enable': 1, 'inverter_enable': 0})
-    
-    inverter_on_msg = setpoint.encode({'negative_torque_limit': 0, 'positive_torque_limit': 0, 'speed_setpoint_rpm': 0, 'remove_error': 0, 'driver_enable': 1, 'hv_enable': 1, 'inverter_enable': 1})
-
-    rpm_msg = setpoint.encode({'negative_torque_limit': -5, 'positive_torque_limit': 5, 'speed_setpoint_rpm': 2000, 'remove_error': 0, 'driver_enable': 1, 'hv_enable': 1, 'inverter_enable': 1})
-    current_rpm = 0
-    # torq_msg = setpoint.encode({'negative_torque_limit': -2, 'positive_torque_limit': 2, 'speed_setpoint_rpm': 100, 'remove_error': 0, 'driver_enable': 1, 'hv_enable': 1, 'inverter_enable': 1})
+    db = cantools.database.load_file("hytech_159.dbc")
+    setpoint = db.get_message_by_name("INV3_STATUS")
 
     error_reset = False
     initialized = False
-    while(1):
-        
-        if(not error_reset):
-            try: 
-                msg = can.Message(arbitration_id=setpoint.frame_id, is_extended_id=False, data = reset_error_msg)
-                bus1.send(msg)
-            except can.CanError:
-                print("Message NOT sent!  Please verify can0 is working first")
-            error_reset = True
+    while(1): 
+        # if recv:
+            # print("Mesg received")  
 
-        if(inverter_ready and inverter_hv_on and inverter_enabled):
-            print("inverter enabled yippee, attempting cmd")
-            try:
-                if(current_rpm < 2000):
-                    msg = can.Message(arbitration_id=setpoint.frame_id, is_extended_id=False, data=rpm_msg)
-                else:
-                    msg = can.Message(arbitration_id=setpoint.frame_id, is_extended_id=False, data=empty_msg)
-                bus1.send(msg)
-            except can.CanError:
-                print("Message NOT sent!  Please verify can0 is working first")
-        elif((not inverter_hv_on) and (inverter_ready) and (not inverter_enabled) and (not initialized)):
-            print(inverter_hv_on)
-            print(inverter_ready)
-            print(inverter_enabled)
-            print("inverter not ready, attempting hv init")
-            try:
-                msg = can.Message(arbitration_id=setpoint.frame_id, is_extended_id=False, data=hv_on_msg)
-                bus1.send(msg)
-            except can.CanError:
-                print("Message NOT sent!  Please verify can0 is working first")
-        elif(inverter_hv_on and inverter_ready and (not inverter_enabled) and (not initialized)):
-            print("inverter ready and hv on but inverter not enabled")
-            try:
-                
-                msg = can.Message(arbitration_id=setpoint.frame_id, is_extended_id=False, data=inverter_on_msg)
-                bus1.send(msg)
-            except can.CanError:
-                print("Message NOT sent!  Please verify can0 is working first")
-        elif((not inverter_hv_on) and (not inverter_ready) and (not inverter_enabled) and (not initialized)):
-            print("first msg")
-            try:
-                
-                msg = can.Message(arbitration_id=setpoint.frame_id, is_extended_id=False, data=empty_msg)
-                bus1.send(msg)
-            except can.CanError:
-                print("Message NOT sent!  Please verify can0 is working first")
+        if inverter_on:
+            print("Inverters are ON")
+        else:
+            print("Inverters are OFF")
 
-        
         rcvd_message = bus1.recv(timeout=0.01)
         if(rcvd_message):
+            recv = True
             try:
                 
                 decoded_message = db.decode_message(rcvd_message.arbitration_id, rcvd_message.data)
@@ -86,11 +37,9 @@ def main():
                 
                 # print(f"Decoded Message: {decoded_message}")
                 
-                if( (msg_info.name.lower() == "mc1_status")):
-                    inverter_ready = decoded_message["system_ready"]
-                    inverter_hv_on = decoded_message["quit_dc_on"]
-                    inverter_enabled = decoded_message["quit_inverter_on"]
-                    current_rpm = abs(decoded_message["speed_rpm"])
+                if( (msg_info.name.lower() == "inv3_status")):
+                    inverter_on = decoded_message["inverter_on"]
+                    # inverter_hv_on = decoded_message["quit_dc_on"]
 
             except KeyError:
                 print(f"Message ID {rcvd_message.arbitration_id} not found in the DBC.")
--- fake_vcf.py
+++ fake_vcf.py
@@ -7,15 +7,25 @@ import os
 
 from enum import Enum
 
-bus1 = can.Bus(channel="can0", interface='socketcan', )
+bus1 = can.Bus(channel="can0", interface='socketcan')
 
 def main():
     db = cantools.database.load_file("hytech_159.dbc")
     pedals_system = db.get_message_by_name("PEDALS_SYSTEM_DATA")
     dash_input = db.get_message_by_name("DASH_INPUT")
 
-    pedals_msg = pedals_system.encode({'brake_pedal': 0.3,
-                                     'accel_pedal': 0,
+    pedals_brake_msg = pedals_system.encode({'brake_pedal': 0,
+                                     'accel_pedal': 0.01,
+                                     'implaus_exceeded_max_duration': False,
+                                     'brake_accel_implausibility': False,
+                                     'mechanical_brake_active': False,
+                                     'accel_pedal_active': False,
+                                     'brake_pedal_active': True,
+                                     'brake_implausible': False,
+                                     'accel_implausible': False})
+    
+    pedals_accel_msg = pedals_system.encode({'brake_pedal': 0,
+                                     'accel_pedal': 0.01,
                                      'implaus_exceeded_max_duration': False,
                                      'brake_accel_implausibility': False,
                                      'mechanical_brake_active': False,
@@ -34,13 +44,47 @@ def main():
                                 'preset_button' : False,
                                 'led_dimmer_button' : False})
 
+    inverter_on = False
+    recv = False
     while(1):
-        msg = can.Message(arbitration_id=pedals_system.frame_id, is_extended_id=False, data = pedals_msg)
+        msg=  can.Message(arbitration_id=pedals_system.frame_id, is_extended_id=False, data = pedals_brake_msg)
         bus1.send(msg)
 
         msg = can.Message(arbitration_id=dash_input.frame_id, is_extended_id=False, data = dash_msg)
         bus1.send(msg)
+
+        if not recv:
+            print("No messages received")
+        elif inverter_on:
+            print("Inverters are ON")
+            time.sleep(1.0)
+            break
+        else:
+            print("Inverters are OFF")
         
+        rcvd_message = bus1.recv(timeout=0.01)
+        if(rcvd_message):
+            recv = False
+            try:
+                decoded_message = db.decode_message(rcvd_message.arbitration_id, rcvd_message.data)
+                msg_info = db.get_message_by_frame_id(rcvd_message.arbitration_id)
+
+                if( (msg_info.name.lower() == "inv3_status")):
+                    inverter_on = decoded_message["inverter_on"]
+            except KeyError:
+                print(f"Message ID {rcvd_message.arbitration_id} not found in the DBC.")
+            except Exception as e:
+                print(f"Error decoding message: {e}")
+
+        time.sleep(.005)
+
+    while(1):
+        msg = can.Message(arbitration_id=pedals_system.frame_id, is_extended_id=False, data = pedals_accel_msg)
+        bus1.send(msg)
+
+        msg = can.Message(arbitration_id=dash_input.frame_id, is_extended_id=False, data = dash_msg)
+        bus1.send(msg)
+
         time.sleep(.005)
 
 if __name__ == "__main__":
